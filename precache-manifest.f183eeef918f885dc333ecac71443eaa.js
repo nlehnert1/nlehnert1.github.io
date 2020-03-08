d4323fa22840c3424430b22e5fa861cdfa7187a6
Chloe Lehnert <chloe.lehnert@gmail.com>
2020-03-08 13:25:00 -0500

    Updates

---
+++ precache-manifest.f183eeef918f885dc333ecac71443eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a873daf21478986a9ce11490490d195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f33554c4c4712111b5c8",
+    "url": "/static/css/main.25162a16.chunk.css"
+  },
+  {
+    "revision": "ced5be2df1b5de63c378",
+    "url": "/static/js/2.d601568a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d601568a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33554c4c4712111b5c8",
+    "url": "/static/js/main.e3f6e359.chunk.js"
+  },
+  {
+    "revision": "7b7355f18ca4e5b8cb26",
+    "url": "/static/js/runtime-main.3bb03058.js"
+  }
+]);
